6c34ef50a6c4716eaf58da218542a6551ce36573
Alex Coronell <alexcoronell@gmail.com>
2023-08-28 20:48:46 -0500

    Admin views created and added to router, header and footer components created

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,28 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: () => import('../views/public/HomeView.vue') },
-    { path: '/experiences', name: 'experiences', component: () => import('../views/public/ExperiencesView.vue') },
-    { path: '/studies', name: 'studies', component: () => import('../views/public/StudiesView.vue') },
-    { path: '/training', name: 'training', component: () => import('../views/public/TrainingView.vue') },
-    { path: '/briefcase', name: 'briefcase', component: () => import('../views/public/BriefcaseView.vue') },
-    { path: '/contact', name: 'contact', component: () => import('../views/public/ContactView.vue') },
-    { path: '/login', name: 'login', component: () => import('../views/public/LoginView.vue') },
+    {
+      path: '/', component: () => import('../views/LayoutView.vue'), children: [
+        { path: '/', name: 'home', component: () => import('../views/public/HomeView.vue') },
+        { path: '/experiences', name: 'experiences', component: () => import('../views/public/ExperiencesView.vue') },
+        { path: '/studies', name: 'studies', component: () => import('../views/public/StudiesView.vue') },
+        { path: '/training', name: 'training', component: () => import('../views/public/TrainingView.vue') },
+        { path: '/briefcase', name: 'briefcase', component: () => import('../views/public/BriefcaseView.vue') },
+        { path: '/contact', name: 'contact', component: () => import('../views/public/ContactView.vue') },
+        { path: '/login', name: 'login', component: () => import('../views/public/LoginView.vue') },
+        {
+          path: '/admin', name: 'admin', component: () => import('../views/admin/AdminLayout.vue'), children: [
+            { path: '', name: 'menu', component: () => import('../views/admin/AdminMenuView.vue') },
+            { path: 'briefcase', name: 'briefcase', component: () => import('../views/admin/AdminBriefcaseView.vue') },
+            { path: 'contact', name: 'contact', component: () => import('../views/admin/AdminContactView.vue') },
+            { path: 'experiences', name: 'experiences', component: () => import('../views/admin/AdminExperiencesView.vue') },
+            { path: 'personal-data', name: 'personal-data', component: () => import('../views/admin/AdminPersonalDataView.vue') },
+            { path: 'studies', name: 'studies', component: () => import('../views/admin/AdminStudiesView.vue') },
+            { path: 'training', name: 'training', component: () => import('../views/admin/AdminTrainingView.vue') },
+          ]
+        }
+      ]
+    }
   ]
 })
 
---
+++ src/views/LayoutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <Header></Header>
+  <RouterView />
+  <Footer></Footer>
+</template>
+
+<script setup>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+</script>
